c90f8f37e5dbc5010bc31cb3f1f2b9a0f1f75627
ripcody <ripcody@gmail.com>
2022-10-13 20:47:07 -0400

    LetterGameReFac-v1

--- app.js
+++ app.js
@@ -1,30 +1,83 @@
-let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let rnd = parseInt(Math.random() * 26);
-let secretLetter = alphabet[rnd];
+let secretLetter = getSecretLetter();
 let numGuesses = 5;
 
 do {
 
-  let usersGuess = prompt('I am thinking of a letter. Can you guess it? You have ' + numGuesses + ' Guess/es.').substring(0, 1).toUpperCase();
+  getUsersGuess();
 
-  if (secretLetter == usersGuess) {
-    alert('Congratulations! The secret letter is ' + secretLetter);
+  if (checkUsersGuess(usersGuess, secretLetter)) {
+    alert(sendUserMessage());
     numGuesses = 0;
   }
 
   else {
 
-    if (secretLetter < usersGuess) {
-      alert('The secret letter is before ' + usersGuess);
+    if (userGuessAfter(usersGuess, secretLetter)) {
+      alert(sendUserMessage());
     }
     else {
-      alert('The secret letter is after ' + usersGuess);
+      alert(sendUserMessage());
     }
 
     numGuesses--;
     if (numGuesses == 0) {
-      alert('You are out of guesses the secret letter is ' + secretLetter);
+      alert(sendUserMessage());
     }
   }
 
 } while (numGuesses > 0);
+playAgain();
+
+
+
+function getSecretLetter() {
+  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let rnd = parseInt(Math.random() * 26);
+  return alphabet[rnd];
+}
+
+function getUsersGuess() {
+  if (numGuesses == 5) {
+    return usersGuess = prompt('I am thinking of a letter. Can you guess it? You have '
+      + numGuesses + ' Guesses.').substring(0, 1).toUpperCase();
+  } else if (numGuesses == 1) {
+    return usersGuess = prompt('Try again you have '
+      + numGuesses + ' Guess left.').substring(0, 1).toUpperCase();
+  } else {
+    return usersGuess = prompt('Try again you have ' + numGuesses +
+      ' Guesses.').substring(0, 1).toUpperCase();
+  }
+}
+
+function checkUsersGuess(usersGuess, secretLetter) {
+  if (usersGuess == secretLetter) {
+    return true;
+  } else
+    return false;
+}
+
+function userGuessAfter(usersGuess, secretLetter) {
+  return secretLetter < usersGuess;
+}
+
+function sendUserMessage() {
+  if (numGuesses == 0) {
+    return 'You are out of guesses the secret letter is ' + secretLetter;
+  }
+  if (usersGuess == secretLetter) {
+    return 'Congratulations! The secret letter is ' + secretLetter;
+  } else if (secretLetter < usersGuess) {
+    return 'The secret letter is before ' + usersGuess;
+  } else {
+    return 'The secret letter is after ' + usersGuess;
+  }
+}
+
+function playAgain() {
+  let plyAgain = prompt('Would you like to play again(Y/N)').substring(0, 1).toUpperCase();
+  if (plyAgain == 'Y') {
+    return location.reload();
+  } else {
+    return numGuesses == 0;
+  }
+}
